c7ddd396882b0474e89f7486c3f0606a9e4273b7
arcnmx <arcnmx@users.noreply.github.com>
2019-09-19 12:53:23 -0700

    allow mkShell to ignore broken/missing packages

--- build-support/shell.nix
+++ build-support/shell.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, self, ... }: lib.mapAttrs (_: lib.flip pkgs.callPackage { }) {
   mkShell = { lib, buildPackages, mkShell, rustPlatform }: {
-    rustfmtDist ? false # use rustfmt from the binary channel rather than from nixpkgs
+    allowBroken ? false # continue even if extra commands are missing or broken
+  , rustfmtDist ? false # use rustfmt from the binary channel rather than from nixpkgs
   , cargoCommands ? [] # e.g. [ "bloat" "binutils" "fmt" "clippy" etc ]
   , rustTools ? [] # [ "llvm-tools" "rust-analyzer" "rls" "xargo" "gdb" "lldb" etc ]
   # TODO: enableRustLld? better to put that in the channel config though...
@@ -10,15 +11,23 @@
       "cargoCommands" "rustTools"
       "nativeBuildInputs"
     ];
+    hasPackage = pkgs: attr: pkgs ? ${attr} &&
+      pkgs.${attr}.meta.broken or false == false &&
+      pkgs.${attr}.meta.available or true == true;
+    tryPackage = pkgs: attr: if allowBroken && !hasPackage pkgs attr
+      then builtins.trace "WARN: rustPlatform.mkShell omitting broken package ${attr}" null
+      else pkgs.${attr};
     mapCargo = c: {
-        "fmt" = if rustfmtDist then rustPlatform.rustfmt else buildPackages.rustfmt;
-        "binutils" = rustPlatform.cargo-binutils;
-        "miri" = rustPlatform.miri;
-        "clippy" = rustPlatform.clippy;
-      }.${c} or buildPackages."cargo-${c}";
+      "fmt" = if rustfmtDist && hasPackage rustPlatform "rustfmt" then
+        tryPackage rustPlatform "rustfmt"
+        else tryPackage buildPackages "rustfmt";
+      "binutils" = tryPackage rustPlatform "cargo-binutils";
+      "miri" = tryPackage rustPlatform "miri";
+      "clippy" = tryPackage rustPlatform "clippy";
+    }.${c} or (tryPackage buildPackages "cargo-${c}");
     mapTool = c: {
-      "rust-analyzer" = buildPackages.rust-analyzer;
-    }.${c} or rustPlatform.${c};
+      "rust-analyzer" = tryPackage buildPackages "rust-analyzer";
+    }.${c} or (tryPackage rustPlatform c);
   in mkShell ({
     nativeBuildInputs = args.nativeBuildInputs or []
       ++ [ rustPlatform.rustc rustPlatform.cargo ]
