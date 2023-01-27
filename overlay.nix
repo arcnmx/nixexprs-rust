0f05e4570097d13147ae5756c91ba9c3a8534d6b
arcnmx <arcnmx@users.noreply.github.com>
2023-01-27 08:12:54 -0800

    bump unstable to 2023-01-27

--- overlay.nix
+++ overlay.nix
@@ -23,8 +23,8 @@ self: super: with super.lib; let
     unstable = rself.distChannel {
       # pinned from https://rust-lang.github.io/rustup-components-history/
       channel = "nightly";
-      date = "2023-01-17";
-      sha256 = "sha256-jUgZNMXkqapR0TKepDB6ZfTszudLWY6+AImVLV3DXMI=";
+      date = "2023-01-27";
+      sha256 = "sha256-JHs8yGthsweFSeamjAt2wzPh4ZeR4dsmHb9tcfT9k5A=";
     };
 
     latest = rself.releases.${lib.last (lib.attrNames rself.releases)};
